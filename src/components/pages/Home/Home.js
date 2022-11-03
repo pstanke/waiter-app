3c8d786e56f1fa62f9d77f392e24672fe4e89bb9
Pawel Stanke <pablostanke21@gmail.com>
2022-11-03 17:53:58 +0100

    Add Home page and Error page to pages

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import { Col, Row } from 'react-bootstrap';
+
+export const Home = () => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>All Posts</h1>
+        </Col>
+      </Row>
+    </>
+  );
+};

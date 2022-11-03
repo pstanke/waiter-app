ce7f853e5fab7070df7ddef60e03dd09c0a22c84
Pawel Stanke <pablostanke21@gmail.com>
2022-11-03 18:04:26 +0100

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-export function App() {}
+import { Container } from 'react-bootstrap';
+
+import { Routes, Route } from 'react-router-dom';
+
+import { Header } from './components/views/Header/Header';
+import { Footer } from './components/views/Footer/Footer';
+import { Home } from './components/pages/Home/Home';
+import { ErrorPage } from './components/pages/ErrorPage/ErrorPage';
+export function App() {
+  return (
+    <Container>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Container>
+  );
+}

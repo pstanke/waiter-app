53e03a8caaf6a7bf7c655ff4b1d5b508dfba1788
Pawel Stanke <pablostanke21@gmail.com>
2022-11-04 11:06:44 +0100

    Add communication with the API server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Container } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 
 import { Header } from './components/views/Header/Header';
 import { Footer } from './components/views/Footer/Footer';
 import { Home } from './components/pages/Home/Home';
 import { ErrorPage } from './components/pages/ErrorPage/ErrorPage';
+
+import { fetchTables } from './redux/tablesRedux';
+
 export function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
     <Container>
       <Header />

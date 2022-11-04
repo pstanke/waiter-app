1da37ddf9c42ef03aed344a837807f4bbdcaed98
Pawel Stanke <pablostanke21@gmail.com>
2022-11-04 12:55:01 +0100

    Add AllTables to features

---
+++ src/components/features/AllTables/AllTables.js
@@ -0,0 +1,28 @@
+import { Button, ListGroup, Row, Col } from 'react-bootstrap';
+
+import { useSelector } from 'react-redux';
+
+import { getAllTables } from '../../../redux/tablesRedux';
+
+export const AllTables = () => {
+  const tables = useSelector(getAllTables);
+  return (
+    <ListGroup variant='flush'>
+      {tables.map((table) => (
+        <ListGroup.Item key={table.id}>
+          <Row className='my-2'>
+            <Col className='d-flex justify-content-start' xs={2}>
+              <h3>Table {table.id}</h3>
+            </Col>
+            <Col className='d-flex align-items-center'>
+              <strong>Status:</strong> {table.status}
+            </Col>
+            <Col className='d-flex justify-content-end '>
+              <Button>Show more</Button>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import { Col, Row } from 'react-bootstrap';
+import { AllTables } from '../../features/AllTables/AllTables';
 
 export const Home = () => {
   return (
@@ -7,6 +8,7 @@ export const Home = () => {
         <Col>
           <h1>All Tables</h1>
         </Col>
+        <AllTables />
       </Row>
     </>
   );
